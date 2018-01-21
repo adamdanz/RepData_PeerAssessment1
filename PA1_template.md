d97cfac2b5a6d2e18dd766ff89a3f4df7615052d
adanz <adam.danz@gmail.com>
2018-01-21 15:37:46 -0500

    New files created to complete assigment.

--- PA1_template.md
+++ PA1_template.md
@@ -8,7 +8,8 @@ output:
 
 ## Loading and preprocessing the data
 ###1. Read data
-```{r}
+
+```r
 currwd <- "/Users/adanz/Dropbox/Danz Cloud/classwork/Data Science - Coursera/5 Reproducible research/week 2/project/Factivity/RepData_PeerAssessment1"
 setwd(currwd)
 activity <- read.csv(unz("activity.zip", "activity.csv"), header=T)
@@ -17,13 +18,17 @@ activity <- read.csv(unz("activity.zip", "activity.csv"), header=T)
 
 ## What is mean total number of steps taken per day?
 ###2. Histogram of total steps per day
-```{r}
+
+```r
 stepsPerDay <- tapply(activity$steps, activity$date, sum, na.rm = TRUE)
 hist(stepsPerDay, main="Steps per day for October, November 2012", xlab="Number of steps per day") 
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-2-1.png)<!-- -->
+
 ###3. Mean & median steps per day
-```{r}
+
+```r
 meanStepsPerDay = mean(stepsPerDay)
 mednStepsPerDay = median(stepsPerDay)
 ```
@@ -31,14 +36,18 @@ mednStepsPerDay = median(stepsPerDay)
 
 ## What is the average daily activity pattern?
 ###4. Time series plot of the avg number of steps per day
-```{r}
+
+```r
 avgIntervals <- tapply(activity$steps, activity$interval, mean, na.rm = TRUE)
 unqIntervals <- unique(activity$interval)
 plot(unqIntervals, avgIntervals, type="l", xlab="time(HHMM)", ylab="avg number of steps")
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-4-1.png)<!-- -->
+
 ###5. The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
+
+```r
 maxIdx = which(avgIntervals==max(avgIntervals))
 maxInterval = unqIntervals[maxIdx]
 ```
@@ -47,14 +56,16 @@ maxInterval = unqIntervals[maxIdx]
 ## Imputing missing values
 ###6. Code to describe and show a strategy for imputing missing data
 #### Here we replace missing data with the average for that time interval across days.
-```{r}
+
+```r
 activityNaIdx <- is.na(activity$steps)
 naTot <- sum(activityNaIdx)
 ```
 
 ###7. Histogram of the total number of steps taken each day after missing values are imputed
 #### Fill in missing vals with mean for that 5-min interval
-```{r}
+
+```r
 naInterval <- activity$interval[activityNaIdx]
 naIntIdx <- match(naInterval, unqIntervals)
 activityNoNa <- activity
@@ -62,13 +73,17 @@ activityNoNa$steps[activityNaIdx] <- avgIntervals[naIntIdx]
 ```
 
 #### display Histogram 
-```{r}
+
+```r
 stepsPerDay2 <- tapply(activityNoNa$steps, activityNoNa$date, sum)
 hist(stepsPerDay2, main="Steps per day for October, November 2012", xlab="Number of steps per day")
 ```
 
+![](PA1_template_files/figure-html/unnamed-chunk-8-1.png)<!-- -->
+
 ### Mean & median steps per day
-```{r}
+
+```r
 meanStepsPerDay2 = mean(stepsPerDay2)
 mednStepsPerDay2 = median(stepsPerDay2)
 ```
@@ -76,7 +91,8 @@ mednStepsPerDay2 = median(stepsPerDay2)
 ## Are there differences in activity patterns between weekdays and weekends?
 ###8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
 #### Create weekday/weekend factors
-```{r}
+
+```r
 dayofweek <- weekdays(as.Date(activityNoNa$date), abbreviate = T)
 isWeekend <- (dayofweek == "Sat") | (dayofweek == "Sun")
 isWeekend <- factor(isWeekend, labels = c("Weekday", "Weekend"))
@@ -84,7 +100,8 @@ activityNoNa$weekday <- isWeekend
 ```
 
 #### Create a time series pannel plot by weekday type
-```{r}
+
+```r
 par(mfrow=c(2,1))
 weekendIdx <- activityNoNa$weekday == "Weekend"
 avgIntervalsWeekday <- tapply(activityNoNa$steps[!weekendIdx], activityNoNa$interval[!weekendIdx], mean, na.rm = TRUE)
@@ -92,5 +109,7 @@ plot(unqIntervals, avgIntervalsWeekday, type="l", xlab="time(HHMM)", ylab="avg n
 avgIntervalsWeekend <- tapply(activityNoNa$steps[weekendIdx], activityNoNa$interval[weekendIdx], mean, na.rm = TRUE)
 plot(unqIntervals, avgIntervalsWeekend, type="l", xlab="time(HHMM)", ylab="avg number of steps", main="Weekends")
 ```
+
+![](PA1_template_files/figure-html/unnamed-chunk-11-1.png)<!-- -->
 
 
--- week2assigment.R
+++ week2assigment.R
@@ -1,96 +1,60 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
+# **Course 5: Reproducible research: week 2 project**
 
-
-## Loading and preprocessing the data
-###1. Read data
-```{r}
+##1. Read data
 currwd <- "/Users/adanz/Dropbox/Danz Cloud/classwork/Data Science - Coursera/5 Reproducible research/week 2/project/Factivity/RepData_PeerAssessment1"
 setwd(currwd)
 activity <- read.csv(unz("activity.zip", "activity.csv"), header=T)
-```
 
-
-## What is mean total number of steps taken per day?
-###2. Histogram of total steps per day
-```{r}
+##2. Histogram of total steps per day
 stepsPerDay <- tapply(activity$steps, activity$date, sum, na.rm = TRUE)
-hist(stepsPerDay, main="Steps per day for October, November 2012", xlab="Number of steps per day") 
-```
+hist(stepsPerDay, main="Steps per day for October, November 2012", xlab="Number of steps per day")
 
-###3. Mean & median steps per day
-```{r}
+##3. Mean & median steps per day
 meanStepsPerDay = mean(stepsPerDay)
 mednStepsPerDay = median(stepsPerDay)
-```
 
-
-## What is the average daily activity pattern?
-###4. Time series plot of the avg number of steps per day
-```{r}
+##4. Time series plot of the avg number of steps per day
 avgIntervals <- tapply(activity$steps, activity$interval, mean, na.rm = TRUE)
 unqIntervals <- unique(activity$interval)
 plot(unqIntervals, avgIntervals, type="l", xlab="time(HHMM)", ylab="avg number of steps")
-```
 
-###5. The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
+##5. The 5-minute interval that, on average, contains the maximum number of steps
 maxIdx = which(avgIntervals==max(avgIntervals))
 maxInterval = unqIntervals[maxIdx]
-```
 
-
-## Imputing missing values
-###6. Code to describe and show a strategy for imputing missing data
-#### Here we replace missing data with the average for that time interval across days.
-```{r}
+##6. Code to describe and show a strategy for imputing missing data
+### Here we replace missing data with the average for that time interval across days.
 activityNaIdx <- is.na(activity$steps)
 naTot <- sum(activityNaIdx)
-```
 
-###7. Histogram of the total number of steps taken each day after missing values are imputed
-#### Fill in missing vals with mean for that 5-min interval
-```{r}
+
+##7. Histogram of the total number of steps taken each day after missing values are imputed
+### Fill in missing vals with mean for that 5-min interval
 naInterval <- activity$interval[activityNaIdx]
 naIntIdx <- match(naInterval, unqIntervals)
 activityNoNa <- activity
 activityNoNa$steps[activityNaIdx] <- avgIntervals[naIntIdx]
-```
-
-#### display Histogram 
-```{r}
+### display Histogram 
 stepsPerDay2 <- tapply(activityNoNa$steps, activityNoNa$date, sum)
 hist(stepsPerDay2, main="Steps per day for October, November 2012", xlab="Number of steps per day")
-```
-
-### Mean & median steps per day
-```{r}
+## Mean & median steps per day
 meanStepsPerDay2 = mean(stepsPerDay2)
 mednStepsPerDay2 = median(stepsPerDay2)
-```
+# Mean and median vals increased 
 
-## Are there differences in activity patterns between weekdays and weekends?
-###8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
-#### Create weekday/weekend factors
-```{r}
+##8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
+### Create weekday/weekend factors
 dayofweek <- weekdays(as.Date(activityNoNa$date), abbreviate = T)
 isWeekend <- (dayofweek == "Sat") | (dayofweek == "Sun")
 isWeekend <- factor(isWeekend, labels = c("Weekday", "Weekend"))
 activityNoNa$weekday <- isWeekend
-```
-
-#### Create a time series pannel plot by weekday type
-```{r}
+### Create a time series pannel plot by weekday type
 par(mfrow=c(2,1))
 weekendIdx <- activityNoNa$weekday == "Weekend"
 avgIntervalsWeekday <- tapply(activityNoNa$steps[!weekendIdx], activityNoNa$interval[!weekendIdx], mean, na.rm = TRUE)
 plot(unqIntervals, avgIntervalsWeekday, type="l", xlab="time(HHMM)", ylab="avg number of steps", main="Weekdays")
 avgIntervalsWeekend <- tapply(activityNoNa$steps[weekendIdx], activityNoNa$interval[weekendIdx], mean, na.rm = TRUE)
 plot(unqIntervals, avgIntervalsWeekend, type="l", xlab="time(HHMM)", ylab="avg number of steps", main="Weekends")
-```
+
 
 
